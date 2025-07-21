74830a53d7cfd91b4fd1e53674ea91d43f32e634
YuforiaCode <2628489679@qq.com>
2025-07-21 18:41:41 +0800

    短信登录-实现短信验证码登录和注册功能

--- src/main/java/com/hmdp/service/IUserService.java
+++ src/main/java/com/hmdp/service/IUserService.java
@@ -1,6 +1,7 @@
 package com.hmdp.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import jakarta.servlet.http.HttpSession;
@@ -19,4 +20,9 @@ public interface IUserService extends IService<User> {
      * 发送短信验证码
      */
     Result sendCode(String phone, HttpSession session);
+
+    /**
+     * 短信验证码登录
+     */
+    Result login(LoginFormDTO loginForm, HttpSession session);
 }
